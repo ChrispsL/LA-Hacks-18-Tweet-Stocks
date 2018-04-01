814f767b3589dc2e5b46365350821d2dd4b1b05e
ChrispsL <christopherwlam@gmail.com>
2018-03-31 20:32:06 -0700

    Merging my data/graph backend

--- server.js
+++ server.js
@@ -54,16 +54,14 @@ app.post('/upload', (req, res) => {
 	} else if(typeof req.body['stock[]'] == 'string') {
 		analysisURL += req.body['stock[]'] + '~100';
 	} else {
-	for(var i = 0; i < req.body['stock[]'].length - 1; i++)
-	{
-		analysisURL = analysisURL + req.body['stock[]'][i] + '~' + (100.0/req.body['stock[]'].length).toString() + '|';
-	};
+		for(var i = 0; i < req.body['stock[]'].length - 1; i++)
+		{
+			analysisURL = analysisURL + req.body['stock[]'][i] + '~' + (100.0/req.body['stock[]'].length).toString() + '|';
+		};
 		analysisURL = analysisURL + req.body['stock[]'][i] + '~' + (100.0/req.body['stock[]'].length).toString();
 }
 
-//var analysisURL = 'https://www.blackrock.com/tools/hackathon/search-securities?identifiers=AAPL';
-	https.get(analysisURL,
-    (res) => {
+	https.get(analysisURL, (res) => {
 		  res.setEncoding('utf8');
 		  let body = "";
 		  res.on("data", data => { body += data });
@@ -71,8 +69,7 @@ app.post('/upload', (req, res) => {
 		  	body = JSON.parse(body);
 			
 			var portfolio = body['resultMap']['PORTFOLIOS'][0]['portfolios'];
-			if(portfolio[0].allDataReturned == false)
-			{
+			if(portfolio[0].allDataReturned == false) {
 				console.log("Invalid!");
 			} else {
 			console.log(portfolio[0]);
@@ -88,6 +85,122 @@ app.post('/upload', (req, res) => {
 		  
     });
 });
+
+
+app.get('/ema', (req, res) => {
+	var performanceURL = 'https://www.blackrock.com/tools/hackathon/performance?identifiers=AMZN'
+	// var performanceURL = 'https://www.blackrock.com/tools/hackathon/portfolio-analysis?positions=AAPL~50|MSFT~50'
+	https.get(performanceURL, (response) => {
+		response.setEncoding('utf8');
+		let body = "";
+		response.on("data", data => { body += data });
+		response.on("end", () => {
+			body = JSON.parse(body);
+			
+			var returns = body.resultMap.RETURNS;
+			console.log(returns);
+			var levels = returns[0].performanceChart.map(function(point) {
+			  				return [point[0], point[1] * 10000]; });
+			var ema5 = movingAverage(5, levels);
+			var ema14 = movingAverage(14, levels);
+			var macd = ema14.map(function(point, i) {
+				return [point[0], ema5[i][1] - point[1]];
+			});
+			var ss = [{
+			  			name: "AMZ Plain",
+			  			data: levels,
+			  			tooltip: { valueDecimals: 2 }
+			  		}, {
+			  			name: "AMZN EMA 5",
+			  			data: ema5,
+			  			tooltip: { valueDecimals: 2 }
+			  		}, {
+			  			name: "AMZN EMA 14",
+			  			data: ema14,
+			  			tooltip: { valueDecimals: 2 }
+			  		}];
+			 var ss2 = [{
+			  			name: "AMZN MACD",
+			  			data: macd,
+			  			tooltip: { valueDecimals: 2 }
+			  		}];
+			res.render("graph", {ss, ss2});
+		});
+	});
+});
+
+app.get('/ema2', (req, res) => {
+	var performanceURL = 'https://www.blackrock.com/tools/hackathon/portfolio-analysis?positions=AAPL~50|MSFT~50&monthsUntilRebalance=50000&useStandardQuarterlyRebalance=false&useStandardYearlyRebalance=false'
+	https.get(performanceURL, (response) => {
+		response.setEncoding('utf8');
+		let body = "";
+		response.on("data", data => { body += data });
+		response.on("end", () => {
+			body = JSON.parse(body);
+			// console.log(body.resultMap.PORTFOLIOS[0].portfolios);
+			var positionName = "";
+
+			
+			var returns = body.resultMap.PORTFOLIOS[0].portfolios[0].returns;
+			var levels = returns.performanceChart.map(function(point) {
+			  				return [point[0], point[1] * 10000]; });
+			var ema5 = movingAverage(5, levels);
+			var ema14 = movingAverage(14, levels);
+			var macd = ema14.map(function(point, i) {
+				return [point[0], ema5[i][1] - point[1]];
+			});
+			var ss = [{
+			  			name: "AAPL MSFT Plain",
+			  			data: levels,
+			  			tooltip: { valueDecimals: 2 }
+			  		}, {
+			  			name: "AAPL MSFT EMA 5",
+			  			data: ema5,
+			  			tooltip: { valueDecimals: 2 }
+			  		}, {
+			  			name: "AAPL MSFT EMA 14",
+			  			data: ema14,
+			  			tooltip: { valueDecimals: 2 }
+			  		}, {
+			  			name: "AAPL MSFT MACD",
+			  			data: macd,
+			  			tooltip: { valueDecimals: 2 }
+			  		}]
+			res.render("graph", {ss});
+			// console.log(ss);
+		});
+	});
+});
+
+
+function movingAverage(period, data)
+{
+	var alpha = 2.0/(period + 1);
+	const memo = [0];
+	var index = 0;
+	return data.map(function(point, i) {
+		if(i < period-1)
+		{
+			return [point[0], null];
+		} else if(i==period) {
+			for(var j = 0; j < period; j++)
+			{
+				memo[index] += point[1];
+			}
+			memo[index] /= period;
+			return [point[0], memo[index]];
+		}
+		memo[index+1] = alpha*point[1] + (1-alpha)*memo[index];
+		index++;
+		return [point[0], memo[index]];
+	});
+}
+
+/*
+EMA -- weighted averages
+ind var: time period
+
+*/
 
 app.listen(3000, () => {
 	console.log('Stock stuff listening on port 3000');
