ab8a586cb9889f432226b271b14e54c972a437ab
jonathanchu78@gmail.com <jonathanchu78@gmail.com>
2018-03-31 20:28:39 -0700

    gets tweets every 20 sec

--- server.js
+++ server.js
@@ -24,22 +24,58 @@ app.get('/newStock', (req, res) => {
 	res.render('partials/StockForm')
 });
 
-app.get('/twitter', (req, res) => {
-	T.get('search/tweets', { q: 'apple since:2018-03-24', count: 100}, (err, data, response) => {
-		for(var i=0; i < data.statuses.length; i++)
-		{
-			console.log(data.statuses[i].text);
-			/*
-				.created_at
-				.truncated (bool)
-				.user[id/id_str/name/screen_name/location/description/followers_count/friends_count]
-				.retweet_count
-			*/
-		}
-	})
-});
 
 
+//if changes by threshold within some time interval, alert
+var threshold = 0.5;
+
+//global array for names of companies
+var companies = new Array();
+var total_sentiments = new Array();
+
+var iteration = 0;
+function getTweets(){
+	for (var k = 0; k < companies.length; k++){
+		T.get('search/tweets', { q: companies[k] + ' since:2018-03-24', count: 100}, (err, data, response) => {
+			console.log(" ");
+			console.log("ITERATION NUMBER " + iteration + " WITH " + data.statuses.length + " TWEETS");
+			console.log(" ");
+			iteration++;
+			for(var i=0; i < data.statuses.length; i++)
+			{
+				//filter for english only posts
+				var wtd_total = 0;
+				var total_wt = 0;
+				if (data.statuses[i].metadata.iso_language_code != "en")
+					continue; //if not english, skip it
+
+				console.log(data.statuses[i].text);
+				console.log(data.statuses[i].user.screen_name + " has " + data.statuses[i].user.followers_count + " followers");
+				if (data.statuses[i].truncated == true)
+					console.log("TRUNCATED");
+				//console.log("language is " + data.statuses[i].metadata.iso_language_code);
+				console.log("------------------------------------");
+
+				var wt = data.statuses[i].user.followers_count;
+				//TODO: GET SENTIMENT
+				/*
+					wtd_total += sentiment * wt;
+					total_wt += wt;
+				*/
+				
+				/*
+					.created_at
+					.truncated (bool)
+					.user[id/id_str/name/screen_name/location/description/followers_count/friends_count]
+					.retweet_count
+				*/
+			}
+		//var total_sentiment = wtd_total / total_wt;
+		//total_sentiments[k] = total_sentiment;
+		})
+	}
+	setTimeout(getTweets, 20000);
+};
 
 app.post('/upload', (req, res) => {
 	// Process
@@ -47,6 +83,7 @@ app.post('/upload', (req, res) => {
 	res.render("index", {title: "Stocks"});
 
 
+	var num_companies = 0;
 	var analysisURL = 'https://www.blackrock.com/tools/hackathon/portfolio-analysis?positions=';
 	if(req.body['stock[]'].length == 0) {
 		console.log("nothing inputted");
@@ -54,12 +91,15 @@ app.post('/upload', (req, res) => {
 	} else if(typeof req.body['stock[]'] == 'string') {
 		analysisURL += req.body['stock[]'] + '~100';
 	} else {
-	for(var i = 0; i < req.body['stock[]'].length - 1; i++)
-	{
-		analysisURL = analysisURL + req.body['stock[]'][i] + '~' + (100.0/req.body['stock[]'].length).toString() + '|';
-	};
+		num_companies++;
+		for(var i = 0; i < req.body['stock[]'].length - 1; i++)
+		{
+			analysisURL = analysisURL + req.body['stock[]'][i] + '~' + (100.0/req.body['stock[]'].length).toString() + '|';
+		};
 		analysisURL = analysisURL + req.body['stock[]'][i] + '~' + (100.0/req.body['stock[]'].length).toString();
-}
+	}
+	num_companies++;
+	console.log("YOU ENTERED " + num_companies + " COMPANIES");
 
 //var analysisURL = 'https://www.blackrock.com/tools/hackathon/search-securities?identifiers=AAPL';
 	https.get(analysisURL,
@@ -75,20 +115,24 @@ app.post('/upload', (req, res) => {
 			{
 				console.log("Invalid!");
 			} else {
-			console.log(portfolio[0]);
-			console.log(portfolio[0]['returns']);
-			console.log(portfolio[0]['returns']['returnsMap']['20180325']);
+			//console.log(portfolio[0]);
+			//console.log(portfolio[0]['returns']);
+			//console.log(portfolio[0]['returns']['returnsMap']['20180325']);
 			// console.log(portfolio[0]['request']);
 			// console.log(portfolio[0]['exposure']);
 			// console.log(portfolio[0]);
-		}
+			}
 		//console.log(body['resultMap']['SEARCH_RESULTS'][0]);
-
+			for (var k = 0; k < num_companies; k++){
+				var company = portfolio[0]['holdings'][k]['description'];
+				companies.push(company);
+				console.log("COMPANY " + k + " " + company);
+	    	}
+			getTweets();
 		  });
-		  
     });
 });
-
+//getTweets(req.body['stock[]']);
 app.listen(3000, () => {
 	console.log('Stock stuff listening on port 3000');
 });
